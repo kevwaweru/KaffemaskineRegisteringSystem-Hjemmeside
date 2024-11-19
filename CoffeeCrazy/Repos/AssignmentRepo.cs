aadfe97bd240af58e7d456ec7a3d937710df9f0c
Kevin <68694478+Mbuzinous@users.noreply.github.com>
2024-11-19 12:21:09 +0100

    Delete assignment

--- CoffeeCrazy/Repos/AssignmentRepo.cs
+++ CoffeeCrazy/Repos/AssignmentRepo.cs
@@ -1,6 +1,50 @@
-﻿namespace CoffeeCrazy.Repos
+﻿using CoffeeCrazy.Interfaces;
+using CoffeeCrazy.Model;
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.PortableExecutable;
+
+namespace CoffeeCrazy.Repos
 {
     public class AssignmentRepo
     {
+        private readonly string _connectionString;
+
+        public AssignmentRepo(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'VaskEnTidDB' not found.");
+        }
+
+        public async Task DeleteAsync(Assignment toBeDeletedAssignment)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+          
+                await connection.OpenAsync();
+                
+                string sqlQuery = "DELETE FROM Assignments WHERE AssignmentId = @AssignmentId";
+
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                command.Parameters.AddWithValue("@AssignmentId", toBeDeletedAssignment.AssignmentId);
+                
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine("Error: " + sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
