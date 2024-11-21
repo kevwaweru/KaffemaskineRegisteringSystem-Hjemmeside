70fce6d754c8f5692eee0c9d1b8e526f325228f5
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2024-11-21 12:31:55 +0100

    udkommenteret det som alle har med - private readonly string & ctor

--- CoffeeCrazy/Repos/MachineRepo.cs
+++ CoffeeCrazy/Repos/MachineRepo.cs
@@ -14,6 +14,17 @@
 
 
 
+        //private readonly string _connectionString;
+        //public MachineRepo(IConfiguration configuration)
+        //{
+        //    _connectionString = configuration.GetConnectionString("DefaultConnection")
+        //        ?? throw new InvalidOperationException("Connection string 'Kaffe Maskine Database' not found.");
+        //}
+
+
+
+
+
 
 
 
