bce1c02e8ab5365998008df1ab7bc36871d4d704
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2024-11-21 10:44:27 +0100

    Done, delete er tilføjet til repo

--- CoffeeCrazy/Repos/MachineRepo.cs
+++ CoffeeCrazy/Repos/MachineRepo.cs
@@ -1,20 +1,52 @@
 ﻿using CoffeeCrazy.Interfaces;
 using CoffeeCrazy.Model;
+using Microsoft.Data.SqlClient;
 
 namespace CoffeeCrazy.Repos
 {
     public class MachineRepo : ICRUDRepo<Machine>
     {
+
+        private readonly string _connectionString;
+        public MachineRepo(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'VaskEnTidDB' not found.");
+        }
+
+
+        public async Task DeleteAsync(Machine toBeDeletedMachine)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    string sqlQuery = "DELETE FROM Machines WHERE MachineId = @MachineId";
+
+                    SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                    command.Parameters.AddWithValue("@MachineId", toBeDeletedMachine.MachineId);
+
+                    await connection.OpenAsync();
+
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine("Error: " + sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
         public Task CreatAsyncc(Machine toBeCreatedT)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteAsync(Machine toBeDeletedT)
-        {
-            throw new NotImplementedException();
-        }
-
+    
         public Task<List<Machine>> GetAllAsync()
         {
             throw new NotImplementedException();
