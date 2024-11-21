0a34804c1e75bf83ebeebd949cd330790a697830
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2024-11-21 12:05:36 +0100

    Rettet til i main og AssignmentRepo

--- CoffeeCrazy/Repos/AssignmentRepo.cs
+++ CoffeeCrazy/Repos/AssignmentRepo.cs
@@ -7,16 +7,16 @@ using System.Reflection.PortableExecutable;
 
 namespace CoffeeCrazy.Repos
 {
-    public class AssignmentRepo
+    public class AssignmentRepo : ICRUDRepo<Assignment>
     {
         private readonly string _connectionString;
+       
 
         public AssignmentRepo(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection")
                 ?? throw new InvalidOperationException("Connection string 'Kaffe Maskine Database' not found.");
         }
-
 
 
         //This Method Creates a Assignment
@@ -84,11 +84,15 @@ namespace CoffeeCrazy.Repos
             }
         }
 
+        public Task<List<Assignment>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
 
-
-
-
-
+        public Task<Assignment> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
 
         //CRUD - Update method.
         public async Task UpdateAsync(Assignment assignmentToBeUpdated)
@@ -103,24 +107,24 @@ namespace CoffeeCrazy.Repos
                 }
 
                 //We use our connection string again, and with using, we don't have to use the dispose method.
-                using (SqlConnection connection = new SqlConnection(_connectionstring))
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
                     //I use the @ to make the string query in a "verbatim string literal", which means the code is read as if in one line and can span multiple lines. Easier to read.
                     //I then write the SQL commands necessary to update the table.
 
                     string query = @"
-               Update Assignments
-               Set 
-                   Titel = @Titel,
-                   Comment = @Comment,
-                   CreateDate = @CreateDate, 
-                   IsCompleted = @IsCompleted,
-               Where
-                   AssignmentId = @AssignmentId";
+                      Update Assignments
+                      Set 
+                          Titel = @Titel,
+                          Comment = @Comment,
+                          CreateDate = @CreateDate, 
+                          IsCompleted = @IsCompleted,
+                      Where
+                          AssignmentId = @AssignmentId";
                     using (SqlCommand command = connection.CreateCommand())
                     {
                         command.Parameters.AddWithValue("@AssignmentId", assignmentToBeUpdated.AssignmentId);
-                        command.Parameters.AddWithValue("@Titel", assignmentToBeUpdated.Titel);
+                        command.Parameters.AddWithValue("@Titel", assignmentToBeUpdated.Title);
                         command.Parameters.AddWithValue("@Comment", (object?)assignmentToBeUpdated.Comment);
                         command.Parameters.AddWithValue("@CreateDate", assignmentToBeUpdated.CreateDate);
                         command.Parameters.AddWithValue("@IsCompleted", assignmentToBeUpdated.IsCompleted);
@@ -140,28 +144,6 @@ namespace CoffeeCrazy.Repos
             {
                 Console.WriteLine("Error" + ex);
             }
-
-
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
