f855d39b3ea21d1490e7b4f4358f0ed97fb09278
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2024-11-21 10:40:43 +0100

    Oprettet

--- CoffeeCrazy/Repos/MachineRepo.cs
+++ CoffeeCrazy/Repos/MachineRepo.cs
@@ -1,6 +1,33 @@
-﻿namespace CoffeeCrazy.Repos
+﻿using CoffeeCrazy.Interfaces;
+using CoffeeCrazy.Model;
+
+namespace CoffeeCrazy.Repos
 {
-    public class MachineRepo
+    public class MachineRepo : ICRUDRepo<Machine>
     {
+        public Task CreatAsyncc(Machine toBeCreatedT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Machine toBeDeletedT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Machine>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Machine toBeUpdatedT)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
