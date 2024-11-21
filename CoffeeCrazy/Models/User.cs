a6720ed22347e5d70240a7c709488798735d967d
Kevin <68694478+Mbuzinous@users.noreply.github.com>
2024-11-21 11:09:07 +0100

    Update Skulle gerne virke

--- CoffeeCrazy/Models/User.cs
+++ CoffeeCrazy/Models/User.cs
@@ -7,8 +7,9 @@ namespace CoffeeCrazy.Model
     {
         public int UserId { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }    
-        public string Passowrd { get; set; }
+        public string LastName { get; set; } 
+        public string Email { get; set; }
+        public string Password { get; set; }
         public Role Role { get; set; }
         public Campus Campus { get; set; }
     }
--- CoffeeCrazy/Repos/UserRepo.cs
+++ CoffeeCrazy/Repos/UserRepo.cs
@@ -1,6 +1,73 @@
-﻿namespace CoffeeCrazy.Repos
+﻿using CoffeeCrazy.Interfaces;
+using CoffeeCrazy.Model;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace CoffeeCrazy.Repos
 {
-    public class UserRepo
+    public class UserRepo : IUserRepo
     {
+        private readonly string _connectionstring;
+
+        public UserRepo(IConfiguration configuration)
+        {
+            _connectionstring = configuration.GetConnectionString("DefaultConnection");
+        }
+        public Task CreateAsync(User toBeCreatedT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(User toBeDeletedT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(User toBeUpdatedT)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionstring))
+                {
+                    string query = "@Update Users SET FirstName = @FirstName, LastName = @LastName, Email = @Email, Password = @Password, RoleId = @RoleId, CampusId = @CampusId WHERE Id = @Id";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Id", (int)toBeUpdatedT.UserId);
+                    command.Parameters.AddWithValue("@FirstName", toBeUpdatedT.FirstName);
+                    command.Parameters.AddWithValue("@LastName", toBeUpdatedT.LastName);
+                    command.Parameters.AddWithValue("@Email", toBeUpdatedT.Email);
+                    command.Parameters.AddWithValue("@Password", toBeUpdatedT.Password);
+                    command.Parameters.AddWithValue("@RoleId", (int)toBeUpdatedT.Role);
+                    command.Parameters.AddWithValue("@CampusId", (int)toBeUpdatedT.Campus);
+
+                    await connection.OpenAsync();
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+            catch (SqlException sqlEx) 
+            {
+                Console.WriteLine("Error:" + sqlEx);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex);
+            }
+
+        }
     }
 }
