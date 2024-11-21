40d2ce6f17affc2dcf14427903e48bd8490990ab
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2024-11-21 11:34:54 +0100

    Jeg commiter

--- CoffeeCrazy/Pages/Users/Index.cshtml.cs
+++ CoffeeCrazy/Pages/Users/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using CoffeeCrazy.Model;
+using CoffeeCrazy.Repos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,38 @@ namespace CoffeeCrazy.Pages.Users
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UserRepo _userRepository;
+
+        public List<User> Users { get; private set; }
+        // ville bruge det til at tage id med. men det bliver jo httpContext der klare det.
+      //  public User SelectedUser { get; private set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? Id { get; set; } 
+
+        public IndexModel(UserRepo userRepository)
         {
+            _userRepository = userRepository;
+        }
+
+        public async Task OnGetAsync()
+        {
+
+            Users = await _userRepository.GetAllAsync();
+
+   //en del af Tanken men det der Seleted User.
+            //if (Id.HasValue)
+            //{
+            //    try
+            //    {
+            //        SelectedUser = await _userRepository.GetByIdAsync(Id.Value);
+            //    }
+            //    catch
+            //    {
+             
+            //        SelectedUser = null;
+            //    }
+            //}
         }
     }
 }
