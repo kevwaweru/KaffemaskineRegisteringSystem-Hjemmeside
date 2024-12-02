e6242cbfd92dedb537dfedc6d4bfa6e692899b00
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2024-12-02 10:57:41 +0100

    Lige rettter navne ORD!

--- CoffeeCrazy/Repos/JobRepo.cs
+++ CoffeeCrazy/Repos/JobRepo.cs
@@ -34,9 +34,8 @@ namespace CoffeeCrazy.Repos
                                     VALUES 
                                      (@TaskTemplateId, @Comment, @CreateDate, @Deadline, @IsCompleted, @MachineId, @UserId, @FrequencyId)";
 
-                    //TaskId og TaskTemplateId skal ikke sendes med pga. de contraints vi har lavet vel? E.g. (1,1).
                     using var command = new SqlCommand(SQLquery, connection);
-                    command.Parameters.AddWithValue("@TaskTemplateId", task.TaskTemplateId);
+                    command.Parameters.AddWithValue("@TaskTemplateId", task.JobTemplateId);
                     command.Parameters.AddWithValue("@Comment", task.Comment);
                     command.Parameters.AddWithValue("@CreateDate", task.CreatedDate);
                     command.Parameters.AddWithValue("@Deadline", task.Deadline);
@@ -65,17 +64,17 @@ namespace CoffeeCrazy.Repos
         }
 
 
-        public async Task DeleteAsync(Job toBeDeletedAssignment)
+        public async Task DeleteAsync(Job toBeDeletedJob)
         {
             try
             {
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    string sqlQuery = "DELETE FROM Assignments WHERE TaskId = @TaskId";
+                    string sqlQuery = "DELETE FROM Tasks WHERE TaskId = @TaskId";
 
                     SqlCommand command = new SqlCommand(sqlQuery, connection);
 
-                    command.Parameters.AddWithValue("@TaskId", toBeDeletedAssignment.TaskId);
+                    command.Parameters.AddWithValue("@TaskId", toBeDeletedJob.JobId);
 
                     await connection.OpenAsync();
 
@@ -95,16 +94,16 @@ namespace CoffeeCrazy.Repos
         /// <summary>
         /// Den metode opdatere en assingment
         /// </summary>
-        /// <param name="assignmentToBeUpdated">Angiv hvilke opgave der skal opdateres</param>
+        /// <param name="JobToBeUpdated">Angiv hvilke opgave der skal opdateres</param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"> den kaster excetion hvis du er dårlig til at kalde den.</exception>
-        public async Task UpdateAsync(Job assignmentToBeUpdated)
+        public async Task UpdateAsync(Job JobToBeUpdated)
         {
             try
             {
-                if (assignmentToBeUpdated == null)
+                if (JobToBeUpdated == null)
                 {
-                    throw new ArgumentNullException(nameof(assignmentToBeUpdated), "Du bliver nødt til at sende ny data med, hvis du vil have opdateret opgaven.");
+                    throw new ArgumentNullException(nameof(JobToBeUpdated), "Du bliver nødt til at sende ny data med, hvis du vil have opdateret opgaven.");
                 }
 
                 using (SqlConnection connection = new SqlConnection(_connectionString))
@@ -112,6 +111,7 @@ namespace CoffeeCrazy.Repos
                     string query = @"
                       Update Tasks
                       Set 
+                          TaskTemplateId = @TaskTemplateId,
                           Comment = @Comment,
                           CreateDate = @CreateDate, 
                           Deadline = @Deadline, 
@@ -123,16 +123,16 @@ namespace CoffeeCrazy.Repos
                           TaskId = @TaskId";
                     using (SqlCommand command = connection.CreateCommand())
                     {
-                        command.Parameters.AddWithValue("@TaskId", assignmentToBeUpdated.TaskId);
-                        command.Parameters.AddWithValue("@Comment", (object?)assignmentToBeUpdated.Comment);
-                        command.Parameters.AddWithValue("@CreateDate", assignmentToBeUpdated.CreatedDate);
-                        command.Parameters.AddWithValue("@Deadline", assignmentToBeUpdated.Deadline);
-                        command.Parameters.AddWithValue("@IsCompleted", assignmentToBeUpdated.IsCompleted);
-                        command.Parameters.AddWithValue("@UserId", assignmentToBeUpdated.UserId);
-                        command.Parameters.AddWithValue("@FrequencyId", assignmentToBeUpdated.FrequencyId);
+                        command.Parameters.AddWithValue("@TaskTemplateId", JobToBeUpdated.JobTemplateId);
+                        command.Parameters.AddWithValue("@Comment", (object?)JobToBeUpdated.Comment);
+                        command.Parameters.AddWithValue("@CreateDate", JobToBeUpdated.CreatedDate);
+                        command.Parameters.AddWithValue("@Deadline", JobToBeUpdated.Deadline);
+                        command.Parameters.AddWithValue("@IsCompleted", JobToBeUpdated.IsCompleted);
+                        command.Parameters.AddWithValue("@UserId", JobToBeUpdated.UserId);
+                        command.Parameters.AddWithValue("@FrequencyId", JobToBeUpdated.FrequencyId);
 
                         connection.Open();
-                        await command.ExecuteNonQueryAsync(); //
+                        await command.ExecuteNonQueryAsync(); 
 
                     }
 
@@ -150,7 +150,6 @@ namespace CoffeeCrazy.Repos
 
         public async Task<List<Job>> GetAllAsync()
         {
-            // Initialize a list to store the jobs.
             var jobs = new List<Job>();
 
             try
@@ -159,8 +158,7 @@ namespace CoffeeCrazy.Repos
                 {
                     await connection.OpenAsync();
 
-                    // Query to retrieve all jobs from the Tasks table.
-                    const string getJobsQuery = @"
+                    string getJobsQuery = @"
                 SELECT TaskId, TaskTemplateId, Comment, CreatedDate, Deadline, 
                        IsCompleted, MachineId, UserId, FrequencyId 
                 FROM Tasks";
@@ -174,8 +172,8 @@ namespace CoffeeCrazy.Repos
                                 // Create a new Job object and populate it with data from the database.
                                 var job = new Job
                                 {
-                                    TaskId = reader.GetInt32(0),
-                                    TaskTemplateId = reader.GetInt32(1),
+                                    JobId = reader.GetInt32(0),
+                                    JobTemplateId = reader.GetInt32(1),
                                     Comment = reader.IsDBNull(2) ? null : reader.GetString(2),
                                     CreatedDate = reader.GetDateTime(3),
                                     Deadline = reader.GetDateTime(4),
@@ -218,7 +216,7 @@ namespace CoffeeCrazy.Repos
                     await connection.OpenAsync();
 
                     // SQL query to get a task by ID.
-                    const string query = @"
+                     string query = @"
                 SELECT TaskId, TaskTemplateId, Comment, CreatedDate, Deadline, 
                        IsCompleted, MachineId, UserId, FrequencyId 
                 FROM Tasks 
@@ -236,8 +234,8 @@ namespace CoffeeCrazy.Repos
                             {
                                 return new Job
                                 {
-                                    TaskId = reader.GetInt32(0),
-                                    TaskTemplateId = reader.GetInt32(1),
+                                    JobId = reader.GetInt32(0),
+                                    JobTemplateId = reader.GetInt32(1),
                                     Comment = reader.IsDBNull(2) ? null : reader.GetString(2),
                                     CreatedDate = reader.GetDateTime(3),
                                     Deadline = reader.GetDateTime(4),
