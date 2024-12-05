82232dae4a780aa34cdc514a2132ae0fa3054504
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2024-12-05 12:05:07 +0100

    Overhånd KAST!

---
+++ CoffeeCrazy/Pages/Machines/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeCrazy.Pages.Machines
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
