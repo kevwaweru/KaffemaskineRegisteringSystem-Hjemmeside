7978ce42ac12bb9a2501d0f513fb05f95afc2f1a
Kevin Waweru <kew001@edu.zealand.dk>
2024-12-11 12:14:06 +0100

    x

--- CoffeeCrazy/Pages/Users/Index.cshtml.cs
+++ CoffeeCrazy/Pages/Users/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using CoffeeCrazy.Interfaces;
 using CoffeeCrazy.Models;
+using CoffeeCrazy.Models.Enums;
 using CoffeeCrazy.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,23 +9,41 @@ namespace CoffeeCrazy.Pages.Users
 {
     public class IndexModel : PageModel
     {
-        public List<User> Users { get; private set; }
-
-        private readonly IUserRepo _UserRepo;
+        private readonly IUserRepo _userRepo;
         private readonly IAccessService _accessService;
-        public IndexModel(IUserRepo userCrudRepository, IAccessService accessService)
+
+        public List<User> Users { get; private set; } = new List<User>();
+        public List<User> FilteredUsers { get; private set; } = new List<User>();
+
+        [BindProperty(SupportsGet = true)]
+        public Campus? CampusFilter { get; set; }
+
+        public IndexModel(IUserRepo userRepo, IAccessService accessService)
         {
-            _UserRepo = userCrudRepository;
+            _userRepo = userRepo;
             _accessService = accessService;
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
             if (!_accessService.IsUserLoggedIn(HttpContext))
+            {
                 return RedirectToPage("/Login/Login");
+            }
+
             try
             {
-                Users = await _UserRepo.GetAllAsync();
+                // Hent brugere
+                Users = await _userRepo.GetAllAsync();
+
+                // Standard til Campus Roskilde, hvis CampusFilter er null
+                if (!CampusFilter.HasValue)
+                {
+                    CampusFilter = Campus.Roskilde;
+                }
+
+                // Filtrer brugere baseret på valgt Campus
+                FilteredUsers = Users.Where(user => user.Campus == CampusFilter).ToList();
 
                 return Page();
             }
